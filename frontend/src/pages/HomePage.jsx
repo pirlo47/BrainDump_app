aa65dc3e9613917a929ef9f6dc92f00a57c4443d
pirlo47 <ntoahaep108@gmail.com>
2026-01-19 11:40:09 +0200

    refactor: restructure HomePage layout for improved note display

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -43,6 +43,20 @@ const HomePage = () => {
     <Navbar />
 
     {isRateLimited && <RateLimitedUI />}
+
+    <div className="max-w-7xl mx-auto p-4 mt-6">
+      {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+      {notes.length > 0 && !isRateLimited && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {notes.map((notes) => (
+            <div>
+              {notes.title} | {notes.content}
+            </div>
+          )) }
+        </div>
+      )}
+    </div>
   </div>
 };
 
